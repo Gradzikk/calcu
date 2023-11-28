a7d12cf8a209881a074c7fe3f67a25ee55490380
wikto <wikto@DESKTOP-NDAD44Q>
2023-11-28 22:52:50 +0100

    Dodaj pliki projektów.

---
+++ calcu/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace calcu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ calcu/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace calcu;
+public partial class MainPage : ContentPage
+{
+    private double currentNumber;
+    private double previousNumber;
+    private string currentOperator;
+    private List<string> history = new List<string>();
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnNumberButtonClicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var number = button.Text;
+        displayLabel.Text += number;
+        currentNumber = double.Parse(displayLabel.Text);
+    }
+
+    private void OnOperatorButtonClicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        currentOperator = button.Text;
+        previousNumber = currentNumber;
+        displayLabel.Text = "";
+    }
+
+    private void OnEqualsButtonClicked(object sender, EventArgs e)
+    {
+        switch (currentOperator)
+        {
+            case "+":
+                currentNumber = previousNumber + currentNumber;
+                break;
+            case "-":
+                currentNumber = previousNumber - currentNumber;
+                break;
+            case "*":
+                currentNumber = previousNumber * currentNumber;
+                break;
+            case "/":
+                currentNumber = previousNumber / currentNumber;
+                break;
+            case "^":
+                currentNumber = Math.Pow(previousNumber, currentNumber);
+                break;
+        }
+        history.Add($"{previousNumber} {currentOperator} {currentNumber} = {currentNumber}");
+        displayLabel.Text = currentNumber.ToString();
+    }
+
+    private void OnClearButtonClicked(object sender, EventArgs e)
+    {
+        currentNumber = 0;
+        previousNumber = 0;
+        currentOperator = null;
+        displayLabel.Text = "";
+    }
+
+    private void OnHistoryButtonClicked(object sender, EventArgs e)
+    {
+        
+            DisplayAlert("Historia", string.Join(Environment.NewLine, history), "OK");
+        
+    }
+}
